eef99caab88c18cf7067f0a416b14f25eabe12f9
frek <frek.mown@gmail.com>
2019-12-24 23:32:15 +0530

    css sourcebutton by achal

--- src/components/SourceButton.js
+++ src/components/SourceButton.js
@@ -1,28 +1,58 @@
-import React, {useContext} from "react";
-import WebContext from "./WebContext";
+import React from "react";
 import Colors from "../constants/Colors";
 
 export default function SourceButton(props) {
-  const context = useContext(WebContext);
-
-  // const radiusCircle = 35 + 4*context.vh;
+  
 
   const styles = {
     screen: {
-      backgroundColor: Colors.headerBackground,
-      borderRadius: 3,
-      alignSelf: "center",
-      justifyContent: "center",
+      // backgroundColor: Colors.headerTitle,
+      backgroundColor: props.title==="Youtube" ? "black" : "white",
+      borderRadius: 10,
+      // alignSelf: "center",
+      // justifyContent: "center",
+      // alignItems: "center",
+      marginTop: "10px",
+      padding: "5px",
+      //background: #eaeaea,
+      // minHeight: "100vh",
+      display: "flex",
       alignItems: "center",
-      marginTop: 5*context.vh,
-      padding: 5,
+      justifyContent: "center",
+      height: "100px",
+      width: "100vw",
+
+      /* Flex Fallback */
+      //marginLeft: "5vw",
+      //marginRight: "5vw",
+      //flex: 1 1 0.2*context.vw,
     },
-  
+
     text: {
-      fontSize: 18,
-      color: props.textColor || Colors.headerTitle
+      fontSize: 30,
+      color: Colors.headerBackground,
+      fontWeight: "bold",
     }
   };
+
+  // const radiusCircle = 35 + 4*context.vh;
+
+  // const styles = {
+  //   screen: {
+  //     backgroundColor: Colors.headerBackground,
+  //     borderRadius: 3,
+  //     alignSelf: "center",
+  //     justifyContent: "center",
+  //     alignItems: "center",
+  //     marginTop: 5*context.vh,
+  //     padding: 5,
+  //   },
+
+  //   text: {
+  //     fontSize: 18,
+  //     color: props.textColor || Colors.headerTitle
+  //   }
+  // };
   return (
     <div onClick={props.onClick}>
       <div style={styles.screen}>
--- src/screens/SelectSourceScreen.js
+++ src/screens/SelectSourceScreen.js
@@ -8,6 +8,7 @@ export default function SelectSourceScreen() {
 
   const styles = {
     screen: {
+      
     }
   };
 
